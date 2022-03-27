da0a4dfbcac7d4842cacaf3b4c3985528010ce03
Sergey <pochtijordan@gmail.com>
2022-03-27 18:10:56 +0300

    Initial Commit

---
+++ App Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App Notes
+//
+//  Created by Sergey on 27.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
