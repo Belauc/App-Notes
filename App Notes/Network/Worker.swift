286183fcbb67000f74ce695e8cebc549468aa088
Sergey <sergey.belyantsev@gmail.ru>
2022-05-27 01:46:21 +0300

    исправлены замечания

--- App Notes/Network/Worker.swift
+++ App Notes/Network/Worker.swift
@@ -9,7 +9,7 @@ import Foundation
 
 protocol WorkerType {
     var session: URLSession { get }
-    func fetch(complition: @escaping (Bool, [Note]?) -> Void)
+    func fetch(completion: @escaping (Bool, [Note]?) -> Void)
 }
 
 final class Worker: WorkerType {
@@ -19,25 +19,26 @@ final class Worker: WorkerType {
         self.session = session
     }
 
-    func fetch(complition: @escaping (Bool, [Note]?) -> Void) {
-        let task = session.dataTask(with: createURLReComponents()!) { data, response, error in
+    func fetch(completion: @escaping (Bool, [Note]?) -> Void) {
+        guard let url = createURLComponents() else { return }
+        let task = session.dataTask(with: url) { data, response, error in
             if let error = error {
                 print(error.localizedDescription)
-                complition(false, nil)
+                completion(false, nil)
                 return
             } else {
                 guard let response = response as? HTTPURLResponse,
                       response.statusCode == 200,
                       let data = data else {
                     let response = response as? HTTPURLResponse
-                    print("Ошибка, код ответа сервера: \(response?.statusCode ?? 0)")
+                    print(self.getErrorMessageFor(statusCode: response?.statusCode ?? 0) ?? "")
                     return
                 }
                 let jsonDecoder = JSONDecoder()
                 jsonDecoder.dateDecodingStrategy = .secondsSince1970
                 do {
                     let notes = try jsonDecoder.decode([Note].self, from: data)
-                    complition(true, notes)
+                    completion(true, notes)
                 } catch let error {
                     print(error)
                 }
@@ -46,15 +47,25 @@ final class Worker: WorkerType {
         task.resume()
     }
 
-    private func createURLReComponents() -> URL? {
+    private func createURLComponents() -> URL? {
         var urlComponents = URLComponents()
         urlComponents.scheme = "https"
         urlComponents.host = "firebasestorage.googleapis.com"
         urlComponents.path = "/v0/b/ios-test-ce687.appspot.com/o/Empty.json"
         urlComponents.queryItems = [
             URLQueryItem(name: "alt", value: "media"),
-            URLQueryItem(name: "token", value: "d07f7d4a-141e-4ac5-a2d2-cc936d4e6f18")
+            URLQueryItem(name: "toen", value: "d07f7d4a-141e-4ac5-a2d2-cc936d4e6f18")
         ]
         return urlComponents.url
+    }
+
+    private func getErrorMessageFor(statusCode: Int) -> String? {
+        if (400...499).contains(statusCode) {
+            return "Извините, что то пошло не так. Попробуйте позже. Ошибка: \(statusCode)"
+        } else if (500...599).contains(statusCode) {
+            return "Извините, сервер временно недоступен. Ошибка: \(statusCode)"
+        } else {
+            return "Какая то ошибочка... \(statusCode)"
+        }
     }
 }
